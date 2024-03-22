b3c194f56259ca749812fd7f7d4e2124854f6882
santhosh_modi <127044374+santhosh436@users.noreply.github.com>
2024-03-22 19:44:46 +0530

    Add files via upload

---
+++ pop_box/script.js
@@ -0,0 +1,9 @@
+let Popup = document.querySelector(".pop")
+// console.log(Popup)
+function openPop(){
+    // console.log(Popup)
+    Popup.classList.add('openPop')
+}
+function closePop(){
+    Popup.classList.remove("openPop")
+}
